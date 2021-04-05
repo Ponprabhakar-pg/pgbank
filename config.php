32e533aa2ddab035ca3fdc83b33d78e9070a240f
Prabhakar <TOSHIBA@Prabhakar.rJ>
2021-04-05 14:31:25 +0530

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect("localhost","id13148209_random","E?%r&Ad33p?E-]=v","id13148209_login");
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
